aa5bc1fc05f72cc82fe3d72e56b4fcbfb38d7836
Abinandhan-2007 <kvabhinanthan@gmail.com>
2026-07-17 14:17:45 +0530

    fix: add BrowserRouter to main.jsx to resolve useNavigate crash

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* Your actual Google Client ID is now active! */}
     <GoogleOAuthProvider clientId="36068887731-dr0cebo46nkuegaj8fto7rbam4lb0cfe.apps.googleusercontent.com">
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>,
 )
